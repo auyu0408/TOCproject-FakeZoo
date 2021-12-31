f6e368e7e08c8c9eab896fdd969809198fdb82b2
auyu <datealive102164@gmail.com>
2021-12-31 20:33:40 +0800

    feat: add send_image_message

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import os
 
 from linebot import LineBotApi, WebhookParser
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
 
 
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
@@ -13,6 +13,15 @@ def send_text_message(reply_token, text):
 
     return "OK"
 
+def send_image_message(reply_token, url):
+    line_bot_api = LineBotApi(channel_access_token)
+    message = ImageSendMessage(
+        original_content_url = url,
+        preview_image_url = url
+    )
+    line_bot_api.reply_message(reply_token, message)
+
+    return "OK"
 
 """
 def send_image_url(id, img_url):
